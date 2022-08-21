40c78e4538d2a6a58814be0638cf376b89594a99
Danilo de Souza Nunes <dannilosn@hotmai.com>
2022-08-21 09:06:43 -0300

    Using ceil instead of floor

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,6 +1,6 @@
 module.exports = {
   randomIntNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1))
+    return Math.ceil(Math.random() * (max - min + 1))
   },
   async sleep(from, to) {
     return await new Promise(resolve => setTimeout(resolve, 1000 * this.randomIntNumber(from, to)))
