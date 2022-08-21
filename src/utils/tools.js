7e66fb1f92e8c9ce356da146e02b0e535252df52
Danilo de Souza Nunes <dannilosn@hotmai.com>
2022-08-21 08:54:07 -0300

    Added return on randomIntNumber method

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,6 +1,6 @@
 module.exports = {
   randomIntNumber(min, max) {
-    Math.floor(Math.random() * (max - min + 1))
+    return Math.floor(Math.random() * (max - min + 1))
   },
   async sleep(from, to) {
     return await new Promise(resolve => setTimeout(resolve, 1000 * this.randomIntNumber(from, to)))
